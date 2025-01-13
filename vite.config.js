70232a74fb19fd2b62c3e266e2c864998628aaf4
Justine <paralejasjustine15@gmail.com>
2025-01-13 16:30:26 +0800

    add build config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,32 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "esnext",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("@firebase")) {
+              return "firebase";
+            }
+            if (id.includes("react")) {
+              return "react";
+            }
+            if (id.includes("framer-motion")) {
+              return "framer-motion";
+            }
+            if (id.includes("popmotion")) {
+              return "popmotion";
+            }
+            if (id.includes("recharts")) {
+              return "recharts";
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
