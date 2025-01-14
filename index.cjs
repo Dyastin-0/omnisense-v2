a964ecd8b12eaac4a5e3258ab5ec0960b4f4afb7
Ubuntu <ubuntu@ip-172-31-38-89.ec2.internal>
2025-01-14 07:17:12 +0000

    Fix base route

--- index.cjs
+++ index.cjs
@@ -5,7 +5,7 @@ const dotenv = require("dotenv").config();
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
