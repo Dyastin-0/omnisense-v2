cea20a8b2af0c72d03dbdaf9191f16e5229e4c96
Justine <paralejasjustine15@gmail.com>
2025-01-19 22:01:27 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -43,10 +43,6 @@ else
 	echo "$APP: Service started"
 fi
 
-echo "$APP: Restarting Caddy server..."
-sudo systemctl restart caddy
-echo "$APP: Caddy restarted"
-
 echo "$APP: Service build and deployment complete"
 
 sudo systemctl status $SERVICE_FILE
