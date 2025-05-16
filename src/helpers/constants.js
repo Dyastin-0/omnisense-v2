3d82ac110e035cc18edc21a2bb8bb353abcb5f63
Justine <paralejasjustine15@gmail.com>
2025-05-16 16:28:07 +0800

    feat: sensor option

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,4 +1,8 @@
-export const SENSORS = ["Sound sensor (KY-038)", "Motion sensor (HC-SR501)"];
+export const SENSORS = [
+  "Sound sensor (KY-038)",
+  "Motion sensor (HC-SR501)",
+  "Flame sensor (KY-026)",
+];
 
 export const GPIO_PINS = [
   15, 2, 0, 4, 16, 17, 5, 18, 19, 21, 22, 23, 13, 12, 14, 27, 26, 25, 33, 32,
